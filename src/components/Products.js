1acb216a0e27c765b9662aa75ad83854a95ad32e
CodewithHadi <hamadazam9636@gmail.com>
2026-01-21 10:19:18 -0800

    fixes

--- src/components/Products.js
+++ src/components/Products.js
@@ -100,30 +100,30 @@ const Products = () => {
 
   fetchProducts();
 }, []);
-    const renderStars = (rating) => {
-    const fullStars = Math.floor(rating);
-    const hasHalfStar = rating % 1 !== 0;
-    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+  //   const renderStars = (rating) => {
+  //   const fullStars = Math.floor(rating);
+  //   const hasHalfStar = rating % 1 !== 0;
+  //   const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
 
-    let starsHTML = [];
+  //   let starsHTML = [];
     
-    // Add full stars
-    for (let i = 0; i < fullStars; i++) {
-      starsHTML.push(<span key={`full-${i}`} className="star full">★</span>);
-    }
+  //   // Add full stars
+  //   for (let i = 0; i < fullStars; i++) {
+  //     starsHTML.push(<span key={`full-${i}`} className="star full">★</span>);
+  //   }
     
-    // Add half star
-    if (hasHalfStar) {
-      starsHTML.push(<span key="half" className="star half">★</span>);
-    }
+  //   // Add half star
+  //   if (hasHalfStar) {
+  //     starsHTML.push(<span key="half" className="star half">★</span>);
+  //   }
     
-    // Add empty stars
-    for (let i = 0; i < emptyStars; i++) {
-      starsHTML.push(<span key={`empty-${i}`} className="star empty">★</span>);
-    }
+  //   // Add empty stars
+  //   for (let i = 0; i < emptyStars; i++) {
+  //     starsHTML.push(<span key={`empty-${i}`} className="star empty">★</span>);
+  //   }
     
-    return starsHTML;
-  };
+  //   return starsHTML;
+  // };
 
   return (
     <section className="products">
